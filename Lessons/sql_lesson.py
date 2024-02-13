bd9bbea6b8fdf7e6ea728f69e1c4d0ddbd44c0f8
Skoteinos1 <p.nagy.11@gmail.com>
2024-02-13 13:38:32 +0000

    update

--- Lessons/sql_lesson.py
+++ Lessons/sql_lesson.py
@@ -1,3 +1,40 @@
 import sqlite3
 
-conn = sqlite3.connect('database.db')
+conn = sqlite3.connect('database.db')  # Connect/create db
+cursor = conn.cursor()  # we nned this to move arround
+
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS user(
+               id INTEGER PRIMARY KEY AUTOINCREMENT,
+               name VARCHAR(255),
+               grade INTEGER
+    );
+''')
+conn.commit() # saves db
+
+name = 'Jack'
+grade = 80
+
+cursor.execute('''
+    CREATE TABLE user(
+               INSERT INTO student(name, grade)
+               VALUES (?,?)
+    );
+''', (name, grade))
+conn.commit()
+
+people = [
+    ('Harry', 50),
+    ('Tom', 70),
+    ('Bob', 80),
+]
+
+# cursor.arraysize
+
+
+# GET VALUES
+cursor.execute('''
+    SELECT * FROM student
+''')
+print(cursor.fetchall())
